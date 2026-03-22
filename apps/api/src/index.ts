db828fd414f92c770b182b8afadd2e31b99b8423
rahuldev7583 <rahuldev7583@gmail.com>
2026-03-22 22:46:26 +0530

    api initialize

---
+++ apps/api/src/index.ts
@@ -0,0 +1,13 @@
+
+import express from "express";
+
+const app = express();
+
+app.get("/api/health", (req, res) => {
+    res.json({ message: "Server is running" })
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+
+})
